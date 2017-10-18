eef27ffbeb1a60b791fb972044b118eaf15f77ed
TerenceChenACL <terence.chen.acl@gmail.com>
2017-10-18 12:49:19 -0700

    first commit
    add ASP.NET project to repo

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialEvent.Startup))]
+namespace SocialEvent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
